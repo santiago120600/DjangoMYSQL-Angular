64845d5e208b5e85a1778cd5d7096f1a69d05caa
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-08-10 12:36:02 -0500

    employee crud and image save

--- DjangoAPI/EmployeeApp/views.py
+++ DjangoAPI/EmployeeApp/views.py
@@ -5,6 +5,8 @@ from django.http import JsonResponse
 
 from EmployeeApp.models import Departments, Employees
 from EmployeeApp.serializers import DepartmentSerializers, EmployeeSerializer
+
+from django.core.files.storage import default_storage
 # Create your views here.
 
 @csrf_exempt
@@ -32,3 +34,37 @@ def departamentApi(request,id=0):
         department = Departments.objects.get(DepartmentId=id)
         department.delete()
         return JsonResponse("Deleted successfully", safe=False)
+
+
+@csrf_exempt
+def employeeApi(request,id=0):
+    if request.method == 'GET':
+        employees = Employees.objects.all()
+        employees_serializers = EmployeeSerializer(employees,many=True)
+        return JsonResponse(employees_serializers.data,safe=False)
+    elif request.method=='POST':
+        employee_data = JSONParser().parse(request)
+        employees_serializer = EmployeeSerializer(data=employee_data)
+        if employees_serializer.is_valid():
+            employees_serializer.save()
+            return JsonResponse("Added successfully",safe=False)
+        return JsonResponse("Failed to Add",safe=False)
+    elif request.method=='PUT':
+        employee_data = JSONParser().parse(request)
+        employee = Employees.objects.get(EmployeeId=employee_data['EmployeeId'])
+        employees_serializer = EmployeeSerializer(employee,data=employee_data)
+        if employees_serializer.is_valid():
+            employees_serializer.save()
+            return JsonResponse("Updated successfully",safe=False)
+        return JsonResponse("Failed to update")
+    elif request.method=='DELETE':
+        employee = Employees.objects.get(EmployeeId=id)
+        employee.delete()
+        return JsonResponse("Deleted successfully", safe=False)
+
+@csrf_exempt
+def SaveFile(request):
+    file=request.FILES['file']
+    file_name = default_storage.save(file.name,file)
+    return JsonResponse(file_name,safe=False)
+
